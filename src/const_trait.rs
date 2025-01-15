24e80f4f0d4d786afdad8d00414345ddde306670
Niko Matsakis <nikomat@amazon.com>
2025-01-15 21:36:14 +0000

    give an example that is not constable

--- src/const_trait.rs
+++ src/const_trait.rs
@@ -8,3 +8,23 @@ impl const Defaulted for String {
         String::new()
     }
 }
+
+struct ActiveUser(Option<String>);
+
+impl Defaulted for ActiveUser {
+    fn defaulted() -> Self {
+        match std::env::var("ACTIVE_USER") {
+            Ok(v) => ActiveUser(Some(v)),
+            Err(_) => ActiveUser(None),
+        }
+    }
+}
+
+impl std::fmt::Display for ActiveUser {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.0 {
+            Some(v) => write!(f, "{v}"),
+            None => write!(f, "(no active user)"),
+        }
+    }
+}
