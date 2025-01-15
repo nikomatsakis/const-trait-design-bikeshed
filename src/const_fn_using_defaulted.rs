751c101dc4b9acf4afa47abe6da14535ade36c98
Niko Matsakis <nikomat@amazon.com>
2025-01-15 21:45:22 +0000

    add a not const version

--- src/const_fn_using_defaulted.rs
+++ src/const_fn_using_defaulted.rs
@@ -9,3 +9,12 @@ where
     let default_value = T::defaulted();
     Equals::equals(value, &default_value)
 }
+
+// for comparison purposes:
+pub fn compare_to_default_not_const<T>(value: &T) -> bool
+where
+    T: Defaulted + Equals
+{
+    let default_value = T::defaulted();
+    Equals::equals(value, &default_value)
+}
